d7156723725ad448f058c890b4313a993ef51daf
deadlockeenside <deadlockeenside@proton.me>
2025-10-17 23:01:19 +0300

    Fill demo stock item rep

--- CatWMS.Infrastructure/DemoRepositories/DemoStockItemRepository.cs
+++ CatWMS.Infrastructure/DemoRepositories/DemoStockItemRepository.cs
@@ -1,7 +1,44 @@
-﻿namespace CatWMS.Infrastructure.DemoRepositories;
+﻿using CatWMS.Domain.Entities;
+using CatWMS.Domain.Interfaces;
+using CatWMS.Domain.ValueObjects;
+
+namespace CatWMS.Infrastructure.DemoRepositories;
 
 // Contains demo data
-public sealed class DemoStockItemRepository
+public sealed class DemoStockItemRepository : IStockItemRepository
 {
-    // TODO: implement IStockItemRepository
+    private readonly List<StockItem> _items = new()
+    {
+        new StockItem 
+        { 
+            Id = 1, 
+            Name = Title.Create("Balloon (27'' / 69 cm), Cat with Bow Figure, Satin, 1 pc") 
+        },
+        new StockItem 
+        { 
+            Id = 2, 
+            Name = Title.Create("Balloon (18'' / 46 cm), Round, Cat with Bow, Cream, Satin, 1 pc") 
+        },
+        new StockItem 
+        { 
+            Id = 3, 
+            Name = Title.Create("Balloon (18'' / 46 cm), Round, Cat with Bow, Pink, Satin, 1 pc") 
+        },
+        new StockItem 
+        {
+            Id = 4, 
+            Name = Title.Create("Balloons (12'' / 30 cm), Cat with Bow Print, Assorted Pastel, 25 pcs") 
+        }
+    };
+
+    public async Task<IReadOnlyCollection<StockItem>> GetAllAsync(CancellationToken cancellationToken = default) 
+    {
+        // Network call simulation
+        await Task.Delay(300, cancellationToken);
+
+        // Cancellation check (if the user interrupted the operation)
+        cancellationToken.ThrowIfCancellationRequested();
+
+        return _items.AsReadOnly();
+    }
 }
