97c0980aa0b7595ba39ec29e61b90a80edbd0439
deadlockeenside <deadlockeenside@proton.me>
2025-10-17 21:25:00 +0300

    Add base value object

---
+++ CatWMS.Domain/Common/ValueObject.cs
@@ -0,0 +1,20 @@
+﻿namespace CatWMS.Domain.Common;
+
+public abstract record ValueObject<T>
+{
+    protected ValueObject(T value)
+    {
+        Validate(value);
+        Value = Format(value);
+    }
+
+    public T Value { get; init; }
+
+    protected abstract void Validate(T value);
+
+    protected virtual T Format(T value) => value;
+
+    public static implicit operator T(ValueObject<T> obj) => obj is null ? throw new ArgumentNullException(nameof(obj)) : obj.Value;
+
+    public sealed override string ToString() => Value?.ToString() ?? string.Empty;
+}
