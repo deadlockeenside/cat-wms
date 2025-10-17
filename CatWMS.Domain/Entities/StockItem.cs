a931eb7a4f13279eed56970a906f44638f48c46e
deadlockeenside <deadlockeenside@proton.me>
2025-10-17 22:10:34 +0300

    Add title to stock item

--- CatWMS.Domain/Entities/StockItem.cs
+++ CatWMS.Domain/Entities/StockItem.cs
@@ -1,8 +1,15 @@
 ﻿using CatWMS.Domain.Common;
+using CatWMS.Domain.ValueObjects;
 
 namespace CatWMS.Domain.Entities;
 
 public sealed class StockItem : Entity
 {
-    // TODO: Name
+    private Title _name = null!;
+
+    public required Title Name 
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(Name));
+    }
 }
